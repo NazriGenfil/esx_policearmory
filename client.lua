1e27f63223f089fa6ef828570b30829b32dfec8a
Nazri <abienazri23@gmail.com>
2021-04-29 00:15:06 +0700

    Initial commit

---
+++ client.lua
@@ -0,0 +1,551 @@
+--------------------------------
+------- Created by Hamza -------
+-------------------------------- 
+
+ESX = nil
+
+Citizen.CreateThread(function()
+	while ESX == nil do
+		TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
+		Citizen.Wait(0)
+	end 
+	while ESX.GetPlayerData().job == nil do
+		Citizen.Wait(10)
+	end
+	PlayerData = ESX.GetPlayerData()
+end)
+
+RegisterNetEvent('esx:playerLoaded')
+AddEventHandler('esx:playerLoaded', function(xPlayer)
+	ESX.PlayerData = xPlayer
+end)
+
+RegisterNetEvent('esx:setJob')
+AddEventHandler('esx:setJob', function(job)
+	ESX.PlayerData.job = job
+end)
+
+-- Function for 3D text:
+function DrawText3Ds(x,y,z, text)
+    local onScreen,_x,_y=World3dToScreen2d(x,y,z)
+    local px,py,pz=table.unpack(GetGameplayCamCoords())
+
+    SetTextScale(0.32, 0.32)
+    SetTextFont(4)
+    SetTextProportional(1)
+    SetTextColour(255, 255, 255, 255)
+    SetTextEntry("STRING")
+    SetTextCentre(1)
+    AddTextComponentString(text)
+    DrawText(_x,_y)
+    local factor = (string.len(text)) / 500
+    DrawRect(_x,_y+0.0125, 0.015+ factor, 0.03, 0, 0, 0, 80)
+end
+
+local insideMarker = false
+
+-- Core Thread Function:
+Citizen.CreateThread(function()
+	while true do
+		Citizen.Wait(5)
+		local coords = GetEntityCoords(PlayerPedId())
+		
+		if (ESX.PlayerData.job and ESX.PlayerData.job.name == Config.PoliceDatabaseName) then
+		for k,v in pairs(Config.ArmoryZones) do
+			for i = 1, #v.Pos, 1 do
+				local distance = Vdist(coords, v.Pos[i].x, v.Pos[i].y, v.Pos[i].z)
+				if (distance < 7.0) and insideMarker == false then
+					DrawMarker(Config.ArmoryMarker, v.Pos[i].x, v.Pos[i].y, v.Pos[i].z-0.975, 0.0, 0.0, 0.0, 0.0, 0, 0.0, Config.ArmoryMarkerScale.x, Config.ArmoryMarkerScale.y, Config.ArmoryMarkerScale.z, Config.ArmoryMarkerColor.r,Config.ArmoryMarkerColor.g,Config.ArmoryMarkerColor.b,Config.ArmoryMarkerColor.a, false, true, 2, true, false, false, false)						
+				end
+				if (distance < 1.0) and insideMarker == false then
+					DrawText3Ds(v.Pos[i].x, v.Pos[i].y, v.Pos[i].z, Config.ArmoryDraw3DText)
+					if IsControlJustPressed(0, Config.KeyToOpenArmory) then
+						ESX.TriggerServerCallback('esx_policeArmory:getWeaponState', function(stock) end)
+						PoliceArmory()
+						insideMarker = true
+						Citizen.Wait(500)
+					end
+				end
+			end
+		end
+
+		for k,v in pairs(Config.KevlarZones) do
+			for i = 1, #v.Pos, 1 do
+				local distance = Vdist(coords, v.Pos[i].x, v.Pos[i].y, v.Pos[i].z)
+				if (distance < 7.0) and insideMarker == false then
+				DrawMarker(Config.KevlarMarker, v.Pos[i].x, v.Pos[i].y, v.Pos[i].z-0.975, 0.0, 0.0, 0.0, 0.0, 0, 0.0, Config.KevlarMarkerScale.x, Config.KevlarMarkerScale.y, Config.KevlarMarkerScale.z, Config.KevlarMarkerColor.r,Config.KevlarMarkerColor.g,Config.KevlarMarkerColor.b,Config.KevlarMarkerColor.a, false, true, 2, true, false, false, false)						
+				end
+				if (distance < 1.0 ) and insideMarker == false then
+					DrawText3Ds(v.Pos[i].x, v.Pos[i].y, v.Pos[i].z, Config.KevlarDraw3DText)
+					if IsControlJustPressed(0, Config.KeyToOpenKevlar) then
+						KevlarMenu()
+						insideMarker = true
+						Citizen.Wait(500)
+					end
+				end
+			end
+		end
+
+		for k,v in pairs(Config.AttachmentZones) do
+			for i = 1, #v.Pos, 1 do
+				local distance = Vdist(coords, v.Pos[i].x, v.Pos[i].y, v.Pos[i].z)
+				if (distance < 7.0) and insideMarker == false then
+				DrawMarker(Config.AttachmentsMarker, v.Pos[i].x, v.Pos[i].y, v.Pos[i].z-0.975, 0.0, 0.0, 0.0, 0.0, 0, 0.0, Config.AttachmentsMarkerScale.x, Config.AttachmentsMarkerScale.y, Config.AttachmentsMarkerScale.z, Config.AttachmentsMarkerColor.r,Config.AttachmentsMarkerColor.g,Config.AttachmentsMarkerColor.b,Config.AttachmentsMarkerColor.a, false, true, 2, true, false, false, false)						
+				end
+				if (distance < 1.0 ) and insideMarker == false then
+					DrawText3Ds(v.Pos[i].x, v.Pos[i].y, v.Pos[i].z, Config.AttachmentsDraw3DText)
+					if IsControlJustPressed(0, Config.KeyToOpenAttachments) then
+						AttachmentMenu()
+						insideMarker = true
+						Citizen.Wait(500)
+					end
+				end
+			end
+		end
+		end
+		
+	end
+end)
+
+-- Function for Weapon Armory:
+PoliceArmory = function()
+	local elements = {
+		{ label = Config.WeaponStorage, action = "weapon_menu" },
+		{ label = 'Deposit Item', action = 'put_stock'}
+	}
+	
+	if not IsEntityPlayingAnim(GetPlayerPed(-1), 'mini@repair', 'fixing_a_player', 3) then
+        ESX.Streaming.RequestAnimDict('mini@repair', function()
+            TaskPlayAnim(GetPlayerPed(-1), 'mini@repair', 'fixing_a_player', 8.0, -8, -1, 49, 0, 0, 0, 0)
+        end)
+    end
+	
+	if tonumber(ESX.PlayerData.job.grade) >= Config.RestockGrade then
+		table.insert(elements, {label = Config.RestockWeapon, action = "restock_menu"})
+		table.insert(elements, {label = 'Ambil Item',  action = 'get_stock'})
+	end
+	
+	ESX.UI.Menu.Open('default', GetCurrentResourceName(), "esx_policeArmory_main_menu",
+		{
+			title    = Config.PoliceArmoryTitle,
+			align    = "top-right",
+			elements = elements
+		},
+	function(data, menu)
+		local action = data.current.action
+
+		if action == "weapon_menu" then
+			WeaponMenu()
+		elseif action == "restock_menu" then
+			RestockMenu()
+		elseif action == "put_stock" then
+			OpenPutStocksMenu()
+		elseif action == "get_stock" then
+			OpenGetStocksMenu()
+		end	
+	end, function(data, menu)
+		menu.close()
+		insideMarker = false
+		ClearPedSecondaryTask(GetPlayerPed(-1))
+	end, function(data, menu)
+	end)
+end
+
+function OpenGetStocksMenu()
+	ESX.TriggerServerCallback('esx_policejob:getStockItems', function(items)
+		local elements = {}
+
+		for i=1, #items, 1 do
+			table.insert(elements, {
+				label = 'x' .. items[i].count .. ' ' .. items[i].label,
+				value = items[i].name
+			})
+		end
+
+		ESX.UI.Menu.Open('default', GetCurrentResourceName(), 'stocks_menu', {
+			title    = 'Deposit Basang',
+			align    = 'top-right',
+			elements = elements
+		}, function(data, menu)
+			local itemName = data.current.value
+
+			ESX.UI.Menu.Open('dialog', GetCurrentResourceName(), 'stocks_menu_get_item_count', {
+				title = 'Jumlah'
+			}, function(data2, menu2)
+				local count = tonumber(data2.value)
+
+				if not count then
+					ESX.ShowNotification('Jumlah tidak valid')
+				else
+					menu2.close()
+					menu.close()
+					TriggerServerEvent('esx_policejob:getStockItem', itemName, count)
+
+					Citizen.Wait(300)
+					OpenGetStocksMenu()
+				end
+			end, function(data2, menu2)
+				menu2.close()
+			end)
+		end, function(data, menu)
+			menu.close()
+		end)
+	end)
+end
+
+function OpenPutStocksMenu()
+	ESX.TriggerServerCallback('esx_policejob:getPlayerInventory', function(inventory)
+		local elements = {}
+
+		for i=1, #inventory.items, 1 do
+			local item = inventory.items[i]
+
+			if item.count > 0 then
+				table.insert(elements, {
+					label = item.label .. ' x' .. item.count,
+					type = 'item_standard',
+					value = item.name
+				})
+			end
+		end
+
+		ESX.UI.Menu.Open('default', GetCurrentResourceName(), 'stocks_menu', {
+			title    = 'Inventaris',
+			align    = 'top-right',
+			elements = elements
+		}, function(data, menu)
+			local itemName = data.current.value
+
+			ESX.UI.Menu.Open('dialog', GetCurrentResourceName(), 'stocks_menu_put_item_count', {
+				title = 'Jumlah'
+			}, function(data2, menu2)
+				local count = tonumber(data2.value)
+
+				if not count then
+					ESX.ShowNotification('Jumlah tidak valid')
+				else
+					menu2.close()
+					menu.close()
+					TriggerServerEvent('esx_policejob:putStockItems', itemName, count)
+
+					Citizen.Wait(300)
+					OpenPutStocksMenu()
+				end
+			end, function(data2, menu2)
+				menu2.close()
+			end)
+		end, function(data, menu)
+			menu.close()
+		end)
+	end)
+end
+
+-- Function for splitting string:
+function WeapSplit(inputstr, del)
+    if del == nil then
+            del = "%s"
+    end
+    local t={}
+    for str in string.gmatch(inputstr, "([^"..del.."]+)") do
+            table.insert(t, str)
+    end
+    return t
+end
+
+-- Function to check if player has weapon:
+-- function PedHasWeapon(hash)
+-- 	for k,v in pairs(ESX.GetPlayerData().loadout) do
+-- 		if v.name == hash then
+-- 			return true
+-- 		else
+-- 			return false
+-- 		end
+-- 	end
+-- end
+
+-- function PedHasWeapon(hash)
+-- 	local ped = GetPlayerPed(-1)
+-- 	local haveWeapon = false
+-- 	for k,v in pairs(Config.WeaponsInArmory) do
+-- 		if GetCurrentPedWeapon(ped, hash, false) then
+-- 		   haveWeapon = true
+-- 		end
+-- 	end
+-- end
+
+
+-- Function for Weapon Menu:
+WeaponMenu = function()
+	local storage = nil
+	local elements = {}
+	local ped = GetPlayerPed(-1)
+	ESX.TriggerServerCallback("esx_policeArmory:getWeaponState", function(stock)	
+	local weapons = WeapSplit(stock[1].weapons, ", ")
+	
+	for k,v in pairs(Config.WeaponsInArmory) do
+		local takenOut = false
+		for z,x in pairs(weapons) do
+			if x == v.weaponHash then
+				takenOut = true
+				table.insert(elements,{label = v.label .. " --- "..('<span style="color:red;">%s</span>'):format("Taken out"), weaponhash = v.weaponHash, lendable = false})
+			end
+		end
+		if takenOut == false then
+			table.insert(elements,{label = v.label .. " --- "..('<span style="color:green;">%s</span>'):format("In Stock"), weaponhash = v.weaponHash, lendable = true})
+		end
+	end
+	
+	ESX.UI.Menu.Open('default', GetCurrentResourceName(), "esx_policeArmory_weapon_storage",
+		{
+			title    = Config.WeaponStorageTitle,
+			align    = "top-right",
+			elements = elements
+		},
+	function(data, menu)
+		menu.close()
+		
+		if data.current.lendable == true then
+			local giveAmmo = (GetWeaponClipSize(GetHashKey(data.current.weaponhash)) > 0)
+			if data.current.weaponhash == "WEAPON_STUNGUN" then
+				giveAmmo = false
+			end
+			TriggerServerEvent("esx_policeArmory:weaponTakenOut", data.current.weaponhash, giveAmmo)
+		elseif HasPedGotWeapon(ped, data.current.weaponhash, false)  then
+			local giveAmmo = (GetWeaponClipSize(GetHashKey(data.current.weaponhash)) > 0)
+			if data.current.weaponhash == "WEAPON_STUNGUN" then
+				giveAmmo = false
+			end
+			TriggerServerEvent("esx_policeArmory:weaponInStock", data.current.weaponhash,GetAmmoInPedWeapon(ped,GetHashKey(data.current.weaponhash)),giveAmmo)
+		else
+			ESX.ShowNotification(Config.ContactSuperVisor)
+		end
+		
+	end, function(data, menu)
+		menu.close()
+	end, function(data, menu)
+	end)
+	end)
+end
+
+-- Function for Restock Menu:
+function RestockMenu()
+	local police = {}
+	local elements = {}
+	ESX.TriggerServerCallback("esx_policeArmory:checkPoliceOnline", function(list) police = list end)
+	Citizen.Wait(250)
+	for k,v in pairs(police) do
+		if v.job.name == Config.PoliceDatabaseName then
+			table.insert(elements, {label = v.name, id = v.id})
+		end
+	end
+	if next(elements) ~= nil then
+		ESX.UI.Menu.Open('default', GetCurrentResourceName(), "esx_policeArmory_restock_menu",
+			{
+				title    = Config.RestockWeaponTitle,
+				align    = "top-right",
+				elements = elements
+			},
+		function(data, menu)
+			menu.close()
+			exports['progressBars']:startUI((Config.RestockTimer * 1000), Config.Progress1)
+			Citizen.Wait((Config.RestockTimer * 1000))
+			TriggerServerEvent("esx_policeArmory:restockWeapons",data.current.id)
+		end, function(data, menu)
+			
+			menu.close()
+		end, function(data, menu)
+		end)
+	else
+		ESX.ShowNotification(Config.NoPoliceOnline)
+	end
+end
+
+-- Function for Kevlar Menu:
+function KevlarMenu()
+	local ped = GetPlayerPed(-1)
+	local elements = {}
+	
+	if not IsEntityPlayingAnim(GetPlayerPed(-1), 'mini@repair', 'fixing_a_player', 3) then
+        ESX.Streaming.RequestAnimDict('mini@repair', function()
+            TaskPlayAnim(GetPlayerPed(-1), 'mini@repair', 'fixing_a_player', 8.0, -8, -1, 49, 0, 0, 0, 0)
+        end)
+    end
+	
+	ESX.UI.Menu.Open('default', GetCurrentResourceName(), "esx_policeArmory_kevlar_menu",
+			{
+				title    = Config.PoliceKevlarTitle,
+				align    = "top-right",
+				elements = {
+					{label = Config.Vest1, armor = 150},
+					--{label = Config.Vest2, armor = 50},
+					--{label = Config.Vest3, armor = 75},
+					{label = Config.Vest4, armor = 100},
+					{label = Config.RemoveVest, armor = 0},
+			}
+			},
+		function(data, menu)
+			SetPedArmour(ped,data.current.armor)
+			if data.current.armor == 0 then
+				exports['progressBars']:startUI((Config.RemoveVestTimer * 1000), Config.Progress2)
+				Citizen.Wait((Config.RemoveVestTimer * 1000))
+				SetPedComponentVariation(GetPlayerPed(-1), 9, 0, 0, 0)
+			else
+				exports['progressBars']:startUI((Config.WearVestTimer * 1000), Config.Progress3)
+				Citizen.Wait((Config.RemoveVestTimer * 1000))
+				if data.current.armor == 150 then
+					SetPedComponentVariation(ped, Config.VestVariation1.componentId, Config.VestVariation1.drawableId, Config.VestVariation1.textureId, Config.VestVariation1.paletteId)
+				elseif data.current.armor == 50 then
+					SetPedComponentVariation(ped, Config.VestVariation2.componentId, Config.VestVariation2.drawableId, Config.VestVariation2.textureId, Config.VestVariation2.paletteId)
+				elseif data.current.armor == 75 then
+					SetPedComponentVariation(ped, Config.VestVariation3.componentId, Config.VestVariation3.drawableId, Config.VestVariation3.textureId, Config.VestVariation3.paletteId)
+				elseif data.current.armor == 100 then
+					SetPedComponentVariation(ped, Config.VestVariation4.componentId, Config.VestVariation4.drawableId, Config.VestVariation4.textureId, Config.VestVariation4.paletteId)
+				end
+			end
+						
+			menu.close()
+			insideMarker = false
+			ClearPedSecondaryTask(GetPlayerPed(-1))
+		end, function(data, menu)
+			menu.close()
+			insideMarker = false
+			ClearPedSecondaryTask(GetPlayerPed(-1))
+		end, function(data, menu)
+		end)
+end
+
+-- Function for Attachment menu:
+function AttachmentMenu()
+	local elements = {}
+	local ped = GetPlayerPed(-1)
+			
+	for k,v in pairs(Config.WeaponsInArmory) do
+		if v.attachment == true then
+			table.insert(elements,{label = v.label, weaponhash = v.weaponHash, type = v.type, attachment = v.attachment, flashlight = v.flashlight, scope = v.scope, suppressor = v.suppressor})
+		end
+	end
+
+	ESX.UI.Menu.Open('default', GetCurrentResourceName(), "esx_policeArmory_attachment_menu",
+		{
+			title    = Config.ChooseWeaponTitle,
+			align    = "top-right",
+			elements = elements
+		},
+	function(data, menu)
+			if data.current.weaponhash == data.current.weaponhash then
+				if GetSelectedPedWeapon(ped) == GetHashKey(data.current.weaponhash) then
+					ListOfAttachments(data.current.type, data.current.label, data.current.weaponhash, data.current.attachment, data.current.flashlight, data.current.scope, data.current.suppressor)
+				else
+					ESX.ShowNotification(Config.WeaponMustBeInHand)
+				end
+			end	
+	end, function(data, menu)
+		menu.close()
+		insideMarker = false
+	end, function(data, menu)
+	end)
+end
+
+-- Function for Attachment List Menu:
+function ListOfAttachments(type,name,weaponhash,attachment,flashlight,scope,suppressor)
+	local elements = {}
+	
+	local ped = GetPlayerPed(-1)
+			
+	if flashlight then
+		local state = HasPedGotWeaponComponent(ped, weaponhash, flashlight)
+		local text
+		
+		if state then
+			text = "Flashlight: "..('<span style="color:green;">%s</span>'):format("On")
+		else
+			text = "Flashlight: "..('<span style="color:red;">%s</span>'):format("Off")
+		end
+		
+		table.insert(elements, {
+			label = text,
+			value = flashlight,
+			state = not state
+		})
+	end
+			
+	if scope then
+		local state = HasPedGotWeaponComponent(ped, weaponhash, scope)
+		local text
+		
+		if state then
+			text = "Scope: "..('<span style="color:green;">%s</span>'):format("On")
+		else
+			text = "Scope: "..('<span style="color:red;">%s</span>'):format("Off")
+		end
+		
+		table.insert(elements, {
+			label = text,
+			value = scope,
+			state = not state
+		})
+	end
+			
+	if suppressor then
+		local state = HasPedGotWeaponComponent(ped, weaponhash, suppressor)
+		local text
+		
+		if state then
+			text = "Suppressor: "..('<span style="color:green;">%s</span>'):format("On")
+		else
+			text = "Suppressor: "..('<span style="color:red;">%s</span>'):format("Off")
+		end
+		
+		table.insert(elements, {
+			label = text,
+			value = suppressor,
+			state = not state
+		})
+	end
+
+	ESX.UI.Menu.Open('default', GetCurrentResourceName(), 'policeArmory_list_of_attachments', {
+		title    = Config.AttachmentTitle,
+		align    = 'top-right',
+		elements = elements
+	}, function(data, menu)
+		local newData = data.current
+		
+		if data.current.value == flashlight then
+			if data.current.state then
+				newData.label = "Flashlight: "..('<span style="color:green;">%s</span>'):format("On")
+				GiveWeaponComponentToPed(ped, weaponhash, data.current.value)
+				ESX.ShowNotification(string.format(Config.FlashlightEquipped,name))
+			else
+				newData.label = "Flashlight: "..('<span style="color:red;">%s</span>'):format("Off")
+				RemoveWeaponComponentFromPed(ped, weaponhash, data.current.value)
+				ESX.ShowNotification(string.format(Config.FlashlightRemove,name))
+			end
+		elseif data.current.value == scope then
+			if data.current.state then
+				newData.label = "Scope: "..('<span style="color:green;">%s</span>'):format("On")
+				GiveWeaponComponentToPed(ped, weaponhash, data.current.value)
+				ESX.ShowNotification(string.format(Config.ScopeEquipped,name))
+			else
+				newData.label = "Scope: "..('<span style="color:red;">%s</span>'):format("Off")
+				RemoveWeaponComponentFromPed(ped, weaponhash, data.current.value)
+				ESX.ShowNotification(string.format(Config.ScopeRemove,name))
+			end
+		elseif data.current.value == suppressor then
+			if data.current.state then
+				newData.label = "Suppressor: "..('<span style="color:green;">%s</span>'):format("On")
+				GiveWeaponComponentToPed(ped, weaponhash, data.current.value)
+				ESX.ShowNotification(string.format(Config.SuppressorEquipped,name))
+			else
+				newData.label = "Suppressor: "..('<span style="color:red;">%s</span>'):format("Off")
+				RemoveWeaponComponentFromPed(ped, weaponhash, data.current.value)
+				ESX.ShowNotification(string.format(Config.SuppressorRemove,name))
+			end
+		end
+				
+		newData.state = not data.current.state
+		menu.update({value = data.current.value}, newData)
+		menu.refresh()
+	end, function(data, menu)
+		menu.close()		
+	end)
+end
---
+++ config.lua
@@ -0,0 +1,123 @@
+--------------------------------
+------- Created by Hamza -------
+-------------------------------- 
+
+Config = {}
+
+-- Police Armory Positions:
+Config.ArmoryZones = {
+	PoliceArmory = {
+		Pos = {
+			{x = 451.6,  y = -980.12, z = 30.69},
+			{x = 380.00,  y = -1607.6, z = 29.29},
+		}
+	}
+}
+
+--Police Kevlar Positions:
+Config.KevlarZones = {
+	PoliceKevlar = {
+		Pos = {
+			{x = 1834.85,  y = 2583.79, z = 40.89},
+			{x = 458.01,  y = -992.77, z = 30.69},
+			{x = 377.15,  y = -1605.35, z = 29.29},
+		}
+	}
+}
+
+--Police Attachments Positions:
+Config.AttachmentZones = {
+	PoliceAttachments = {
+		Pos = {
+			{x = 0,  y = 0, z = 0}
+		}
+	}
+}
+
+-- Police Armory Marker Settings:
+Config.KeyToOpenArmory = 38															-- default 38 is E
+Config.ArmoryMarker = 27 															-- marker type
+Config.ArmoryMarkerColor = { r = 50, g = 50, b = 204, a = 100 } 					-- rgba color of the marker
+Config.ArmoryMarkerScale = { x = 1.0, y = 1.0, z = 1.0 }  							-- the scale for the marker on the x, y and z axis
+Config.ArmoryDraw3DText = "Press ~g~[E]~s~ for ~y~Weapon Armory~s~"					-- set your desired text here
+
+-- Police Kevlar Marker Settings:
+Config.KeyToOpenKevlar = 38															-- default 38 is E
+Config.KevlarMarker = 27 															-- marker type
+Config.KevlarMarkerColor = { r = 50, g = 50, b = 204, a = 100 } 					-- rgba color of the marker
+Config.KevlarMarkerScale = { x = 1.0, y = 1.0, z = 1.0 }  							-- the scale for the marker on the x, y and z axis
+Config.KevlarDraw3DText = "Press ~g~[E]~s~ for ~y~Kevlar Armory~s~"					-- set your desired text here
+
+-- Police Attachments Marker Settings:
+Config.KeyToOpenAttachments = 38													-- default 38 is E
+Config.AttachmentsMarker = 27 														-- marker type
+Config.AttachmentsMarkerColor = { r = 50, g = 50, b = 204, a = 100 } 				-- rgba color of the marker
+Config.AttachmentsMarkerScale = { x = 1.0, y = 1.0, z = 1.0 }   					-- the scale for the marker on the x, y and z axis
+Config.AttachmentsDraw3DText = "Press ~g~[E]~s~ for ~y~Attachment Armory~s~"		-- set your desired text here
+
+-- Menu Element Labels:
+Config.WeaponStorage = "Weapons"
+Config.RestockWeapon = "Restock Weapon"
+Config.Vest1 = "Densus"					-- 25% armor
+--Config.Vest2 = "Kevlar 2"					-- 50% armor
+--Config.Vest3 = "Kevlar 3"					-- 75% armor
+Config.Vest4 = "Reskrim"					-- 100% armor
+Config.RemoveVest = "Lepas Rompi"			-- 0% armor
+
+-- Menu Title Labels_
+Config.PoliceArmoryTitle = "Weapon Armory"
+Config.WeaponStorageTitle = "Weapons"
+Config.RestockWeaponTitle = "Restock Weapons"
+Config.PoliceKevlarTitle = "Kevlar"
+Config.ChooseWeaponTitle = "Choose Weapon"
+Config.AttachmentTitle = "Attachments"
+
+-- ESX Show Notifications_
+Config.ContactSuperVisor = "You ~b~have already~s~ taken this ~r~weapon~s~ out, ~y~contact~s~ a ~r~supervisor~s~"
+Config.NoPoliceOnline = "No ~b~Police Officers~s~ are ~g~online~s~"
+Config.WeaponMustBeInHand = "You ~y~must hold~s~ the ~r~weapon~s~ in your hand"
+Config.FlashlightEquipped = "You ~b~equipped~s~ a ~y~flashlight~s~ for ~r~%s ~s~"
+Config.FlashlightRemove = "You ~b~removed~s~ a ~y~flashlight~s~ for ~r~%s ~s~"
+Config.ScopeEquipped = "You ~b~equipped~s~ a ~y~scope~s~ for ~r~%s ~s~"
+Config.ScopeRemove = "You ~b~removed~s~ a ~y~scope~s~ for ~r~%s ~s~"
+Config.SuppressorEquipped = "You ~b~equipped~s~ a ~y~scope~s~ for ~r~%s ~s~"
+Config.SuppressorRemove = "You ~b~removed~s~ a ~y~scope~s~ for ~r~%s ~s~"
+
+-- ProgressBar Text_
+Config.Progress1 = "RESTOCKING WEAPONS"
+Config.Progress2 = "REMOVING VEST"
+Config.Progress3 = "WEARING VEST"
+
+-- ProgressBar Timer, in seconds:
+Config.RestockTimer = 1
+Config.RemoveVestTimer = 2
+Config.WearVestTimer = 2
+
+-- Ped Component Variation (Body Armor that player will wear)
+Config.VestVariation1 = { componentId = 9, drawableId = 5, textureId = 0, paletteId = 5 } -- 25%
+--Config.VestVariation2 = { componentId = 9, drawableId = 1, textureId = 0, paletteId = 0 } -- 50%
+--Config.VestVariation3 = { componentId = 9, drawableId = 1, textureId = 2, paletteId = 0 } -- 75%
+Config.VestVariation4 = { componentId = 9, drawableId = 1, textureId = 0, paletteId = 1 } -- 100%
+
+-- Set the exact name from your jobs database for police
+Config.PoliceDatabaseName = 'police'
+
+-- Set required police job grade to restock weapon
+Config.RestockGrade = 11
+-- Set amount of ammo you want when taking a weapon out
+Config.AmmountOfAmmo = 250
+
+-- Here u add weapons and its components (set nil at a component to disable it).
+Config.WeaponsInArmory = {
+	{ weaponHash = 'WEAPON_FLASHLIGHT', label = 'Senter', type = 'other', attachment = false, flashlight = nil, scope = nil, suppressor = nil},
+	{ weaponHash = 'WEAPON_NIGHTSTICK', label = 'Nightstick', type = 'other', attachment = false, flashlight = nil, scope = nil, suppressor = nil  },
+	{ weaponHash = 'WEAPON_STUNGUN', label = 'Taser', type = 'other', attachment = false, flashlight = nil, scope = nil, suppressor = nil  },
+	--{ weaponHash = 'WEAPON_PISTOL', label = 'Pistol', type = 'pistol', attachment = true, flashlight = 0x43FD595B, scope = nil, suppressor = 0x65EA7EBB},
+	{ weaponHash = 'weapon_assaultsmg', label = 'Assault SMG', type = 'submachine ', attachment = true, flashlight = 0x43FD595B, scope = nil, suppressor = 0x65EA7EBB},
+	{ weaponHash = 'WEAPON_PISTOL50', label = 'Pistol 50', type = 'pistol50', attachment = true, flashlight = 0x7BC4CDDC, scope = 0x3CC6BA57, suppressor = 0xC304849A  },
+	{ weaponHash = 'WEAPON_SPECIALCARBINE', label = 'Special Carbine', type = 'rifle', attachment = true, flashlight = 0x7BC4CDDC, scope = 0x49B2945, suppressor = 0x837445AA  },
+	{ weaponHash = 'weapon_carbinerifle', label = 'Carbine Rifle', type = 'rifle', attachment = true, flashlight = 0x7BC4CDDC, scope = nil, suppressor = nil  },
+	{ weaponHash = 'WEAPON_PUMPSHOTGUN', label = 'Less Lethal Shotgun', type = 'rifle', attachment = true, flashlight = 0x7BC4CDDC, scope = 0x49B2945, suppressor = 0x837445AA  },
+}
+
+Config.DiscordWebook = "https://discord.com/api/webhooks/809814750871158874/D5xjljoDwzUmmgEcw1b_5RsDA1LEeMrb31s3TaGGviKgIv_ADbcw2VVrAQWMLXy2nfAF"
